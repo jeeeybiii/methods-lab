113f3191af141328accc043109d5b5ba4c112e28
jeeeybiii <macuj1@student.op.ac.nz>
2026-09-23 12:05:04 +1200

    initial files added

---
+++ Program.cs
@@ -0,0 +1,10 @@
+﻿namespace MethodsLab
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
